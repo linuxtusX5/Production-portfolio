02bfc3ffc0919b206ab247c4ac71f4f5b1f4a036
SalvatusX5 Dev <salvatusjoffrey@gmail.com>
2023-07-15 21:27:06 +0800

    @

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import aboutImg from "../assets/Untitled design.png";
 import { Button } from "react-bootstrap";
-import resume from '../assets/Salvatus Resume.pdf';
+import resume from '../assets/Salvatus Resume (1).pdf';
 const About = () => {
   const info = [
     { text: "experience", count: "Entry level" },
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import project1 from "../assets/p1.png";
 import project2 from "../assets/p2.png";
 import project3 from "../assets/p3.png";
+import project4 from "../assets/t2.png";
+import project5 from "../assets/Screenshot_2023-07-15-20-52-32-13.jpg";
 import "swiper/css";
 import "swiper/css/pagination";
 
@@ -29,6 +30,13 @@ const Project = () => {
               <h5 className="text-xl my-4">Password: <b>123</b></h5>
               <div className="flex gap-3">
                 <a
+                  href='https://github.com/linuxtusX5/Dev-medical-appointment'
+                  target="_blank"
+                  className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
+                >
+                  Repository
+                </a>
+                <a
                   href='https://medical-appointment.onrender.com'
                   target="_blank"
                   className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
@@ -49,6 +57,13 @@ const Project = () => {
               <h5 className="text-xl my-4">Password: <b>Passw0rd</b></h5><br/>
               <div className="flex gap-3">
                 <a
+                  href='https://github.com/linuxtusX5/linuxtusX5.github.io'
+                  target="_blank"
+                  className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
+                >
+                  Repository
+                </a>
+                <a
                   href='https://main--studentcouncil.netlify.app/'
                   target="_blank"
                   className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
@@ -62,11 +77,53 @@ const Project = () => {
               <h3 className="text-xl my-4">Advance Care</h3>
               <div className="flex gap-3">
                 <a
+                  href='https://github.com/linuxtusX5/advancecare'
+                  target="_blank"
+                  className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
+                >
+                  Repository
+                </a>
+                <a
                   href='https://advancecare.netlify.app'
                   target="_blank"
                   className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
                 >
                   Live Demo
+                </a>
+              </div>
+            </div>
+            <div className="h-fit w-full p-4 bg-slate-700 rounded-xl mb-5">
+              <img src={project4} alt="" className="rounded-lg" />
+              <h3 className="text-xl my-4">Todo App</h3>
+              <h5 className="text-xl my-4">Email: <b>yoon@gmail.com</b></h5>
+              <h5 className="text-xl my-4">Password: <b>123</b></h5>
+              <div className="flex gap-3">
+                <a
+                  href='https://github.com/linuxtusX5/Dev-Todo-full-stack'
+                  target="_blank"
+                  className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
+                >
+                  Repository
+                </a>
+                <a
+                  href='https://todo-app-7d9k.onrender.com/signup/login'
+                  target="_blank"
+                  className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </div>
+            <div className="h-fit w-full p-4 bg-slate-700 rounded-xl mb-5">
+              <img src={project5} alt="" className="rounded-lg" style={{height: '50vh', marginLeft: '30vh'}} />
+              <h3 className="text-xl my-4">Task Management </h3>
+              <div className="flex gap-3">
+                <a
+                  href='https://drive.google.com/drive/folders/12X_KBNgMvJdL4XzB_AW51-FdjxhfIsfD?usp=sharing'
+                  target="_blank"
+                  className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
+                >
+                  Download
                 </a>
               </div>
             </div>
